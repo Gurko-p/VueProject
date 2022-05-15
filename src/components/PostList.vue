1b5f375dac202fd6bf082ffcdb6c8f4649e47bf8
Pavel Gurko <gurko-p@mail.ru>
2022-05-15 21:53:31 +0300

    Initial

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h4>Список пользователей: </h4>
+        <post-item v-for='post in posts' :post='post' :key='post.id' @remove="$emit('remove', post)"/>
+    </div>
+</template>
+
+<script>
+import PostItem from '@/components/PostItem'
+export default {
+    components: {
+        PostItem
+    },
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
